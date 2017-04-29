5c5aabb8a88035d414bc3e74d92284143eb6794c
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-04-29 18:36:39 +0900

    Edit_create_action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,8 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    if before_save
-    @group.save
-    GroupUser.create(group_id: @group.id, user_id: current_user.id)
+    if @group.save
+      GroupUser.create(group_id: @group.id, user_id: current_user.id)
       redirect_to root_path
     end
   end
@@ -16,12 +15,6 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name)
-  end
-
-  def before_save
-    if @group.name?
-      return true
-    end
   end
 
 end
