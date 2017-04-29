0355137bb0a0a3980cf9a9572cfb43bdbf42a532
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-04-29 18:10:19 +0900

    Edit_create_action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,21 +1,27 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
   end
 
   def create
-    params = { group: {
-      name: group_params[:name], group_users_attributes: [
-        { user_id: current_user.id }
-      ]
-    }}
-    Group.create(params[:group])
-    redirect_to root_path
+    @group = Group.new(group_params)
+    if before_save
+    @group.save
+    GroupUser.create(group_id: @group.id, user_id: current_user.id)
+      redirect_to root_path
+    end
   end
 
   private
   def group_params
-    params.permit(:name)
+    params.require(:group).permit(:name)
+  end
+
+  def before_save
+    if @group.name?
+      return true
+    end
   end
 
 end
