00701c5bc30d04ed06c64e109890b9415d012b8d
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-04-29 14:41:18 +0900

    Add_create_action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,4 +3,20 @@ class GroupsController < ApplicationController
   def new
   end
 
+  def create
+    binding.pry
+    params = { group: {
+      name: group_params[:name], group_users_attributes: [
+        { user_id: current_user.id }
+      ]
+    }}
+    Group.create(params[:group])
+    redirect_to root_path
+  end
+
+  private
+  def group_params
+    params.permit(:name)
+  end
+
 end
