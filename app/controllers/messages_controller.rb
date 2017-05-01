0f2931b1686d4032206b6c12756c16b0aba3ea45
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-01 16:26:32 +0900

    Edit_index_html_haml

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,9 @@
 class MessagesController < ApplicationController
 
-before_action :move_to_registraion
-
   def index
-    @group = Group.find(params[:group_id])
+    if params[:group_id]
+      @group = Group.find(params[:group_id])
+    end
     @groups = current_user.groups
   end
-
-  private
-  
-  def move_to_registraion
-    redirect_to new_user_registration_path unless user_signed_in?
-  end
-
 end
