44e201d0fd2fbc53664a87725a81a02049033ad4
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-01 13:43:24 +0900

    Add_move_to_registration

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,15 @@
 class MessagesController < ApplicationController
 
+before_action :move_to_registraion
+
   def index
     @group = Group.find(params[:group_id])
     @group_users = current_user.group_users
   end
+
+private
+def move_to_registraion
+  redirect_to new_user_registration_path unless user_signed_in?
+end
 
 end
