aa17f2211686f6f2b1bdf1f8dc1072feec1d8a26
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-01 22:19:56 +0900

    Edit_message_create_action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,9 +3,22 @@ class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
+    @message = Message.new
   end
 
   def create
-    
+    @message = Message.new(body: message_params[:body], group_id: params[:group_id], user_id: current_user.id)
+    if @message.save
+      redirect_to group_messages_path, notice: 'メッセージ送信成功'
+    else
+      flash.now[:alert] = 'メッセージ送信失敗'
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
