0c427e8dc079d4230b0879c065af7bc7894e4499
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-02 13:03:35 +0900

    Edit_strong_parameter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,12 +4,13 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
     @message = Message.new
-    @messages = @group.messages.all
+    @messages = @group.messages
     @users = @group.users
   end
 
   def create
-    @message = Message.new(body: message_params[:body], group_id: params[:group_id], user_id: current_user.id)
+    a_message_params = {}
+    @message = Message.new(message_params(a_message_params))
     if @message.save
       redirect_to group_messages_path, notice: 'メッセージ送信成功'
     else
@@ -20,7 +21,10 @@ class MessagesController < ApplicationController
 
   private
 
-  def message_params
-    params.require(:message).permit(:body)
+  def message_params(a_message_params)
+    a_message_params[:body] = params.require(:message).permit(:body)[:body]
+    a_message_params[:group_id] = params.permit(:group_id)[:group_id]
+    a_message_params[:user_id] = current_user.id
+    return a_message_params
   end
 end
