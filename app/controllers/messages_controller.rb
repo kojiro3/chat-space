6b9de9e837504ff45f6be758f773669e3b5b9917
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-03 14:27:40 +0900

    Add_before_action_set_instance

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,16 @@
 class MessagesController < ApplicationController
 
-  def index
-    @group = Group.find(params[:group_id])
-    @groups = current_user.groups
-    @message = Message.new
-    @messages = @group.messages
-    @users = @group.users
-  end
+  before_action :set_instance, only: [:index, :create]
+
+  def index; end
 
   def create
     @message = current_user.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path, notice: 'メッセージ送信成功'
     else
-      # flash.now[:alert] = 'メッセージ送信失敗'
-      # render :index
-      redirect_to group_messages_path, notice: "#{@message.errors.full_messages}"
+      flash.now[:alert] = "#{@message.errors.full_messages}"
+      render :index
     end
   end
 
@@ -23,5 +18,13 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body, :image).merge(group_id: params[:group_id])
+  end
+
+  def set_instance
+    @group = Group.find(params[:group_id])
+    @groups = current_user.groups
+    @message = Message.new
+    @messages = @group.messages
+    @users = @group.users
   end
 end
