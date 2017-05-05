e60ad69e294055dfba2a96f1d9653229998005c8
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-05 20:03:40 +0900

    Edit_factories

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,19 +3,5 @@ FactoryGirl.define do
   factory :message do
     body      'hello world!'
     image     'aaaaa'
-    association :group
-    association :user
   end
-
-  factory :group do
-    name 'group1'
-  end
-
-  factory :user do
-    name                'kojiro'
-    email                   'aaa@gmail.com'
-    password                '00000000'
-    password_confirmation   '00000000'
-  end
-
 end
