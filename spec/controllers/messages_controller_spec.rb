4fc00c6972d238d92174b48fd1bdfa19b328806f
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-05 20:07:46 +0900

    Add_example_it_assigns_the_requested_groups_to_@groups

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-describe MessagesController do
-  
+describe MessagesController, type: :controller do
+  let(:user) { create(:user) }
+
+  describe 'GET #index' do
+    before do
+      login_user user
+    end
+
+    it 'assigns the requested groups to @groups' do
+      @group = user.groups.first
+      get :index, params: { group_id: @group.id }
+      groups = user.groups
+      expect(assigns(:groups)).to eq groups
+    end
+  end
 end
