fbae5cc1a084a70b9b196532195e2c01db5edfa2
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-16 12:06:04 +0900

    Add_route_to_messages_search

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :groups, only: [:index, :create, :new, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create] do
+      collection do
+        get 'search'
+      end
+    end
   end
   resources :users, only: [] do
     collection do
