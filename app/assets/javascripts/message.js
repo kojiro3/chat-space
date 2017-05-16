868b5da18da914b4413255184712b4d154cffc16
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-16 12:07:45 +0900

    Edit_message.js_for_auto_reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,18 @@
 $(function() {
+
   function buildHTML(message) {
     var html = {};
     html.body = $('<li class="chat__message">').append(message.body);
     html.date = $('<li class="chat__date">').append(message.created_at);
     html.name = $('<li class="chat__name">').append(message.name)
-    html.image = $(`<image src=${message.image.url} class="chat__image">`)
+    if (message.image.url) {
+      html.image = $(`<image src=${message.image.url} class="chat__image">`)
+    }
     return html;
   }
+
+  var message_ids = [];
+  auto_reload();
 
   $('.js-form').on('submit', function(e) {
     e.preventDefault();
@@ -22,8 +28,7 @@ $(function() {
       contentType: false
     })
     .done(function(data) {
-      var html = buildHTML(data);
-      $('.chat').append(html.name, html.date, html.body, html.image);
+      auto_reload();
       textField.val('');
       $(".send-button").prop("disabled", false);
     })
@@ -32,4 +37,41 @@ $(function() {
       $(".send-button").prop("disabled", false);
     });
   });
+
+  function auto_reload() {
+    var last_id = $(message_ids).last()[0];
+    if (last_id) {
+      $.ajax({
+        type: 'GET',
+        url: './messages/search',
+        data: {
+          last_id: last_id
+        },
+        dataType: 'json'
+      })
+      .done(function(data) {
+        $.each(data, function(i, message) {
+          var html = buildHTML(message);
+          $('.chat').append(html.name, html.date, html.body, html.image);
+          message_ids.push(message.id)
+        });
+      })
+      .fail(function() {
+      });
+    } else {
+      $.ajax({
+        type: 'GET',
+        url: './messages',
+        dataType: 'json'
+      })
+      .done(function(data) {
+        message_ids.push(data.id)
+      })
+      .fail(function() {
+      });
+    }
+  };
+
+  check = setInterval(auto_reload, 10000);
+
 });
