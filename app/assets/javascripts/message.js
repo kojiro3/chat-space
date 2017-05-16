51d15c0ed2aa99f1c446c9e2f228de18d3c5cc99
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-16 15:45:29 +0900

    Edit_message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -11,33 +11,6 @@ $(function() {
     return html;
   }
 
-  var message_ids = [];
-  auto_reload();
-
-  $('.js-form').on('submit', function(e) {
-    e.preventDefault();
-    var textField = $('.right-cont__message__field');
-    var form = $(this).get()[0];
-    var formData = new FormData(form);
-    $.ajax({
-      type: 'POST',
-      url: './messages',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data) {
-      auto_reload();
-      textField.val('');
-      $(".send-button").prop("disabled", false);
-    })
-    .fail(function() {
-      alert('メッセージを入力してください');
-      $(".send-button").prop("disabled", false);
-    });
-  });
-
   function auto_reload() {
     var last_id = $(message_ids).last()[0];
     if (last_id) {
@@ -72,6 +45,31 @@ $(function() {
     }
   };
 
+  var message_ids = [];
+  auto_reload();
   check = setInterval(auto_reload, 10000);
 
+  $('.js-form').on('submit', function(e) {
+    e.preventDefault();
+    var textField = $('.right-cont__message__field');
+    var form = $(this).get()[0];
+    var formData = new FormData(form);
+    $.ajax({
+      type: 'POST',
+      url: './messages',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      auto_reload();
+      textField.val('');
+      $(".send-button").prop("disabled", false);
+    })
+    .fail(function() {
+      alert('メッセージを入力してください');
+      $(".send-button").prop("disabled", false);
+    });
+  });
 });
