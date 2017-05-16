8aa233171d60f0fdfe0e9248a5655eab90934a6a
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-05-16 16:25:50 +0900

    Delete_route_to_messages_search

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :groups, only: [:index, :create, :new, :edit, :update] do
     resources :messages, only: [:index, :create] do
-      collection do
-        get 'search'
-      end
     end
   end
   resources :users, only: [] do
